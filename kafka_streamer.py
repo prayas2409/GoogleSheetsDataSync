6addc21400c4e03438910804967ab7a4aecfdb7b
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-01 19:11:41 +0530

    [Prayas] Refactored: Reading data from all topics based on sheets Kafka server

--- kafka_streamer.py
+++ kafka_streamer.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from kafka.admin import KafkaAdminClient
+from file_list import KafkaTopicCreator
+from utility import space_remover
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,11 +15,13 @@ class KafkaStreamer:
             # .config("spark.jars",)\
         self.spark.sparkContext.setLogLevel("ERROR")
 
-    def stream(self):
+    def stream(self,topics):
+        topics = ",".join(topics) if len(topics) > 0 else topics[0]
+
         df = self.spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers", os.getenv("BOOTSTRAP_SERVERS")) \
-        .option("subscribe", "DEMOSheets").load()
-        
+        .option("subscribe",topics).load()
+
         ds = df \
         .selectExpr("CAST(value AS STRING)") \
         .writeStream \
@@ -28,4 +33,10 @@ class KafkaStreamer:
 
 if __name__ == "__main__":
     kafka_streamer = KafkaStreamer()
-    kafka_streamer.stream()
+    kafka_topic_creator = KafkaTopicCreator(KafkaAdminClient(
+                bootstrap_servers=os.getenv("BOOTSTRAP_SERVERS"), 
+                client_id='test')  
+            )
+    sheets = kafka_topic_creator.get_list_of_sheets(os.getenv("DIR_ID"))
+    sheets = space_remover(sheets)
+    kafka_streamer.stream(sheets)
