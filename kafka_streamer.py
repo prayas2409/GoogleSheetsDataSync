e7d42a2025726716f868e55ffc2c42eccc42a806
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-01-02 10:42:29 +0530

    [Prayas] Added: Functionality to stream as per the schema provided

--- kafka_streamer.py
+++ kafka_streamer.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import *
 from kafka.admin import KafkaAdminClient
 from kafka_topic_creator import KafkaTopicCreator
 from utility import space_remover
@@ -11,21 +12,31 @@ load_dotenv()
 class KafkaStreamer:
 
     def __init__(self):
+        """[Initialize spark for streaming]
+        """        
         self.spark = SparkSession.builder.appName("kafkaStream").master("local[*]").getOrCreate()
-        self.spark.sparkContext.setLogLevel("ERROR")
+        self.spark.sparkContext.setLogLevel("ERROR") # collect logs for spark level > level error
 
-    def stream(self,topics):
+    def stream(self,topics:list,schema:StructType):
+        """[Streams data from kafka as structured streaming]
+
+        Args:
+            topics (list): [List of topics to stream from]
+            schema (StructType): [Schema for the data to be streamed]
+        """        
         topics = ",".join(topics) if len(topics) > 0 else topics[0]
 
         df = self.spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers", os.getenv("BOOTSTRAP_SERVERS")) \
         .option("subscribe",topics).load()
 
-        ds = df \
-        .selectExpr("CAST(value AS STRING)") \
+        val_df = df.selectExpr("CAST(value as STRING)")
+        json_df = val_df.select(from_json(col("value"),schema).alias("data")).select("data.*")
+        ds = json_df \
         .writeStream \
         .format("console") \
         .outputMode("append") \
+        .option("checkpointLocation", "checkpoints") \
         .start() \
         .awaitTermination()
 
@@ -38,4 +49,8 @@ if __name__ == "__main__":
             )
     sheets = kafka_topic_creator.get_list_of_sheets(os.getenv("DIR_ID"))
     sheets = space_remover(sheets)
-    kafka_streamer.stream(sheets)
+    # Defining the structure for the data present in Google sheet
+    schema_demo_sheet= StructType([ StructField("A", StringType(), True),
+                StructField("B", StringType(), True),
+                StructField("C", StringType(), True)])
+    kafka_streamer.stream(sheets,schema_demo_sheet)
